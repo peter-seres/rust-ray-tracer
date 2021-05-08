adaebccc6b600d016edb1fd0713d36316cbb3c1e
pseres <peet.seres@gmail.com>
2021-05-08 10:00:51 +0200

    Added infinite plane to Hittables.

---
+++ src/hittables.rs
@@ -0,0 +1,106 @@
+use crate::{Point, Vector3, Normal, Ray, Scalar, Color};
+
+pub trait Hittable {
+    fn get_normal(&self, p: Point) -> Normal;
+    fn intersect(&self, ray: &Ray) -> Option<(Scalar, Color, Normal)>;
+}
+
+
+pub struct Sphere {
+    origin: Point,
+    radius: Scalar,
+    color: Color
+}
+
+impl Sphere {
+    pub fn new(origin: Vector3, radius: Scalar, color: Color) -> Self {
+        Self{origin, radius, color}
+    }
+}
+
+pub struct InfPlane {
+    origin: Point,
+    normal: Normal,
+    color: Color
+}
+
+impl InfPlane {
+    pub const HIT_EPS: Scalar = 1e-2;
+
+    pub fn new(origin: Vector3, normal: Normal, color: Color) -> Self {
+        Self{origin, normal, color}
+    }
+}
+
+
+impl Hittable for Sphere {
+    fn get_normal(&self, p: Point) -> Normal {
+        p - self.origin
+    }
+
+    fn intersect(&self, ray: &Ray) -> Option<(Scalar, Color, Normal)> {
+
+        // Vector pointing from Sphere origin to Ray origin
+        let sphere_to_ray = ray.origin - self.origin;
+
+        // Second order equation terms:
+        let a: Scalar = ray.dir.dot(&ray.dir);
+        let b: Scalar = 2.0 * sphere_to_ray.dot(&ray.dir);
+        let c: Scalar = sphere_to_ray.dot(&sphere_to_ray) - self.radius * self.radius;
+
+        // Look for solutions of the second order equation:
+        let discriminant: Scalar = b * b - 4.0 * a * c;
+
+        if discriminant < 0.0 {
+            None
+        } else {
+            let mut numerator: Scalar = -b - discriminant.sqrt();
+
+            if numerator > 0.0 {
+                let distance: Scalar = numerator / (2.0 * a);
+                let p = ray.at_distance(distance);
+                let normal = self.get_normal(p);
+                Some((distance, self.color, normal))
+            } else {
+                numerator = -b + discriminant.sqrt();
+
+                if numerator > 0.0 {
+                    let distance: Scalar = numerator / (2.0 * a);
+                    let p = ray.at_distance(distance);
+                    let normal = self.get_normal(p);
+                    Some((distance, self.color, normal))
+                }
+                else {
+                    None
+                }
+            }
+        }
+    }
+}
+
+impl Hittable for InfPlane {
+    fn get_normal(&self, _p: Point) -> Normal {
+        self.normal
+    }
+
+    fn intersect(&self, ray: &Ray) -> Option<(Scalar, Color, Normal)> {
+        let denominator = ray.dir.dot(&self.normal);
+
+        // Check if the ray is parallel with the plane with a threshold epsilon.
+        if denominator.abs() < InfPlane::HIT_EPS {
+            None
+        } else {
+
+            // Find the location of the intersection
+            let lp = self.origin - ray.origin;
+            let nominator = lp.dot(&self.normal);
+            let distance: Scalar = nominator / denominator;
+
+            if distance > 0.0 {
+                Some((distance, self.color, self.normal))
+            } else {
+                None
+            }
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ mod consts;
 mod image;
 mod ray;
 mod types;
+mod hittables;
 
 pub use camera::Camera;
 pub use color::{Color, ColorData};
@@ -11,6 +12,7 @@ pub use consts::*;
 pub use image::Image;
 pub use ray::Ray;
 pub use types::*;
+pub use hittables::{Hittable, Sphere, InfPlane};
 
 // Pre-define a few colors.
 const WHITE: Color = Color::new(1.0, 1.0, 1.0);
@@ -19,70 +21,24 @@ const GREY: Color = Color::new(0.5, 0.5, 0.5);
 const SKYBLUE: Color = Color::new(0.5, 0.7, 1.0);
 const RED: Color = Color::new(1.0, 0.1, 0.1);
 const BLUE: Color = Color::new(0.1, 0.1, 1.0);
+const GREEN: Color = Color::new(0.1, 1.0, 0.1);
 
 
-trait Hittable {
-    fn get_normal(&self, p: Point) -> Normal;
-    fn intersect(&self, ray: &Ray) -> Option<(Scalar, Color, Normal)>;
+// Shader settings:
+const LAMBERT_INT: Scalar = 0.5;
+const AMBIENT_INT: Scalar = 0.9;
+
+
+#[derive(Clone, Copy)]
+struct Light {
+    origin: Point,
 }
 
-struct Sphere {
-    origin: Vector3,
-    radius: Scalar,
-    color: Color
-}
-
-impl Sphere {
-    fn new(origin: Vector3, radius: Scalar, color: Color) -> Self {
-        Self{origin, radius, color}
+impl Light {
+    fn new(origin: Point) -> Self {
+        Self{origin}
     }
 }
-
-impl Hittable for Sphere {
-    fn get_normal(&self, p: Point) -> Normal {
-        p - self.origin
-    }
-
-    fn intersect(&self, ray: &Ray) -> Option<(Scalar, Color, Normal)> {
-
-        // Vector pointing from Sphere origin to Ray origin
-        let sphere_to_ray = ray.origin - self.origin;
-
-        // Second order equation terms:
-        let a: Scalar = ray.dir.dot(&ray.dir);
-        let b: Scalar = 2.0 * sphere_to_ray.dot(&ray.dir);
-        let c: Scalar = sphere_to_ray.dot(&sphere_to_ray) - self.radius * self.radius;
-
-        // Look for solutions of the second order equation:
-        let discriminant: Scalar = b * b - 4.0 * a * c;
-
-        if discriminant < 0.0 {
-            None
-        } else {
-            let mut numerator: Scalar = -b - discriminant.sqrt();
-
-            if numerator > 0.0 {
-                let distance: Scalar = numerator / (2.0 * a);
-                let p = ray.at_distance(distance);
-                let normal = self.get_normal(p);
-                Some((distance, self.color, normal))
-            } else {
-                numerator = -b + discriminant.sqrt();
-
-                if numerator > 0.0 {
-                    let distance: Scalar = numerator / (2.0 * a);
-                    let p = ray.at_distance(distance);
-                    let normal = self.get_normal(p);
-                    Some((distance, self.color, normal))
-                }
-                else {
-                    None
-                }
-            }
-        }
-    }
-}
-
 
 fn get_closest_intersection<const N: usize>(ray: Ray, hittables: &[&dyn Hittable; N]) -> Option<(Point, Color, Normal)> {
     let mut hit: (Scalar, Option<(Color, Normal)>) = (1e3, None);
@@ -106,13 +62,14 @@ fn get_closest_intersection<const N: usize>(ray: Ray, hittables: &[&dyn Hittable
 }
 
 
-fn raycast<const N: usize>(ray: Ray, hittables: &[&dyn Hittable; N]) -> Color {
-    // let sky_scaler = ray.dir[1];
-    // let background: Color = (1.0 - sky_scaler) * WHITE + sky_scaler * SKYBLUE;
 
-    match get_closest_intersection(ray, hittables) {
+fn raycast<const N: usize>(ray: Ray, hittables: &[&dyn Hittable; N], _light: Light) -> Color {
+
+    let mut _hit: Option<(Point, Color, Normal)> = None;
+
+    return match get_closest_intersection(ray, hittables) {
         None => BLACK,
-        Some((_point, color, _normal)) => color
+        Some((_point, color, _normal)) => color * AMBIENT_INT
     }
 }
 
@@ -130,14 +87,18 @@ fn main() {
     let mut color_data = ColorData::new(vec![]);
 
     // Make objects in the scene:
-    const N: usize = 2;                  // number of objects
+    const N: usize = 3;                  // number of objects
     let s1 = Sphere::new(Vector3::new(0.0, 0.0, -5.0), 0.5, RED);
     let s2 = Sphere::new(Vector3::new(1.0, 0.5, -7.0), 0.5, BLUE);
-    let objects: [&dyn Hittable; N] = [&s1, &s2];
+    let p = InfPlane::new(Vector3::new(0.0, -2.0, -5.0), Vector3::new(0.0, 1.0, 0.0), GREEN);
+    let objects: [&dyn Hittable; N] = [&s1, &s2, &p];
+
+    // Make lights in the scene:
+    let light = Light::new(Vector3::new(-1.0, 1.0, 0.0));
 
     // Iterate through the Camera, do ray tracing and gather the color data
     for ray in c {
-        let c: Color = raycast(ray, &objects);
+        let c: Color = raycast(ray, &objects, light);
         color_data.push(c);
     }
 
